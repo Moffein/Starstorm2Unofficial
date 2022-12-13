0dc0d97c15f7b9d7b5c70d23d91313e29a0d6df4
Moffein <22742925+Moffein@users.noreply.github.com>
2022-12-13 15:40:43 -0800

    Cyborg thrusters now activate while sprinting.

--- Starstorm 2/Survivors/Cyborg/EntityStates/CyborgMain.cs
+++ Starstorm 2/Survivors/Cyborg/EntityStates/CyborgMain.cs
@@ -10,6 +10,10 @@ namespace EntityStates.Starstorm2States.Cyborg
         private float hoverAcceleration = 60f;  //was 25f
         private CyborgController cyborgController;
 
+        private Transform thrusterEffectL;
+        private Transform thrusterEffectR;
+        private bool inJetpackState = false;
+
         private EntityStateMachine jetpackStateMachine;
 
         public override void OnEnter()
@@ -21,27 +25,27 @@ namespace EntityStates.Starstorm2States.Cyborg
             ChildLocator cl = base.GetModelChildLocator();
             if (cl)
             {
-                Transform thrusterEffectL = cl.FindChild("ThrusterEffectL");
-                if (thrusterEffectL) thrusterEffectL.gameObject.SetActive(false);
+                thrusterEffectL = cl.FindChild("ThrusterEffectL");
+                if (thrusterEffectL)
+                {
+                    thrusterEffectL.gameObject.SetActive(false);
+                }
 
-                Transform thrusterEffectR = cl.FindChild("ThrusterEffectR");
-                if (thrusterEffectR) thrusterEffectR.gameObject.SetActive(false);
+                thrusterEffectR = cl.FindChild("ThrusterEffectR");
+                if (thrusterEffectR)
+                {
+                    thrusterEffectR.gameObject.SetActive(false);
+                }
             }
-        }
-
-        public override void OnExit()
-        {
-            
-            base.OnExit();
         }
 
         public override void ProcessJump()
         {
             base.ProcessJump();
+            inJetpackState = this.jetpackStateMachine.state.GetType() == typeof(JetpackOn);
             if (this.hasCharacterMotor && this.hasInputBank && base.isAuthority)
             {
                 bool inputPressed = base.inputBank.jump.down && base.characterMotor.velocity.y < 0f && !base.characterMotor.isGrounded;
-                bool inJetpackState = this.jetpackStateMachine.state.GetType() == typeof(JetpackOn);
                 if (inputPressed && !inJetpackState && cyborgController.allowJetpack)
                 {
                     this.jetpackStateMachine.SetNextState(new JetpackOn());
@@ -56,6 +60,33 @@ namespace EntityStates.Starstorm2States.Cyborg
         public override void FixedUpdate()
         {
             base.FixedUpdate();
+
+            inJetpackState = this.jetpackStateMachine.state.GetType() == typeof(JetpackOn);
+            bool shouldShowThruster = (inJetpackState || (base.characterBody && base.characterBody.isSprinting));
+            if (shouldShowThruster)
+            {
+                if (thrusterEffectL)
+                {
+                    thrusterEffectL.gameObject.SetActive(true);
+                }
+
+                if (thrusterEffectR)
+                {
+                    thrusterEffectR.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                if (thrusterEffectL)
+                {
+                    thrusterEffectL.gameObject.SetActive(false);
+                }
+
+                if (thrusterEffectR)
+                {
+                    thrusterEffectR.gameObject.SetActive(false);
+                }
+            }
 
             // rest idle!!
             //if (this.animator) this.animator.SetBool("inCombat", (!base.characterBody.outOfCombat || !base.characterBody.outOfDanger));
--- Starstorm 2/Survivors/Cyborg/EntityStates/JetpackOn.cs
+++ Starstorm 2/Survivors/Cyborg/EntityStates/JetpackOn.cs
@@ -10,28 +10,10 @@ namespace EntityStates.Starstorm2States.Cyborg
         {
             base.OnEnter();
 			Util.PlaySound("Play_mage_m1_impact", base.gameObject);
-
-			childLocator = base.GetModelChildLocator();
-			if (childLocator)
-			{
-				Transform thrusterEffectL = childLocator.FindChild("ThrusterEffectL");
-				if (thrusterEffectL) thrusterEffectL.gameObject.SetActive(true);
-
-				Transform thrusterEffectR = childLocator.FindChild("ThrusterEffectR");
-				if (thrusterEffectR) thrusterEffectR.gameObject.SetActive(true);
-			}
 		}
 
         public override void OnExit()
 		{
-			if (childLocator)
-			{
-				Transform thrusterEffectL = childLocator.FindChild("ThrusterEffectL");
-				if (thrusterEffectL) thrusterEffectL.gameObject.SetActive(false);
-
-				Transform thrusterEffectR = childLocator.FindChild("ThrusterEffectR");
-				if (thrusterEffectR) thrusterEffectR.gameObject.SetActive(false);
-			}
 			base.OnExit();
         }
 
@@ -45,7 +27,6 @@ namespace EntityStates.Starstorm2States.Cyborg
 				base.characterMotor.velocity = new Vector3(base.characterMotor.velocity.x, num, base.characterMotor.velocity.z);
 			}
 		}
-		private ChildLocator childLocator;
 		public static float hoverVelocity = -1f;
 		public static float hoverAcceleration = 60f;
 	}
