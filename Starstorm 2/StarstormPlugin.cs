aecce3f80355c2ec79a6a30c9e64565686925a62
Moffein <22742925+Moffein@users.noreply.github.com>
2022-12-17 00:37:48 -0800

    Updated version number.

--- Starstorm 2/StarstormPlugin.cs
+++ Starstorm 2/StarstormPlugin.cs
@@ -43,7 +43,7 @@ namespace Starstorm2
     {
         internal const string guid = "com.ChirrLover.Starstorm2Unofficial";
         internal const string modName = "Starstorm 2 Unofficial";
-        internal const string version = "0.5.2";
+        internal const string version = "0.5.3";
 
         public static StarstormPlugin instance;
 
