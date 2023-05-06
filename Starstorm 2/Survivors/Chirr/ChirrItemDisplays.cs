ddc5bcf7d37d9db6f0f171c737b6a01f6fe53542
DestroyedClone <destroyedclone@gmail.com>
2023-05-06 03:11:15 -0700

    Added ItemDisplays classes for each survivor for consistency

---
+++ Starstorm 2/Survivors/Chirr/ChirrItemDisplays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using RoR2;
+using Starstorm2Unofficial.Cores;
+using Starstorm2Unofficial.Survivors.Cyborg;
+
+namespace Starstorm2Unofficial.Survivors.Chirr
+{
+    public static class ChirrItemDisplays
+    {
+        public static ItemDisplayRuleSet itemDisplayRuleSet;
+
+        public static List<ItemDisplayRuleSet.KeyAssetRuleGroup> itemRules;
+
+        public static void RegisterDisplays()
+        {
+            GameObject bodyPrefab = ChirrCore.chirrPrefab;
+            GameObject model = bodyPrefab.GetComponentInChildren<ModelLocator>().modelTransform.gameObject;
+            CharacterModel characterModel = model.GetComponent<CharacterModel>();
+
+            itemDisplayRuleSet = ScriptableObject.CreateInstance<ItemDisplayRuleSet>();
+            itemRules = new List<ItemDisplayRuleSet.KeyAssetRuleGroup>();
+
+            #region DisplayRules
+
+            #endregion
+
+
+            ItemDisplayRuleSet.KeyAssetRuleGroup[] item = itemRules.ToArray();
+            itemDisplayRuleSet.keyAssetRuleGroups = item;
+            //itemDisplayRuleSet.GenerateRuntimeValues();
+
+            characterModel.itemDisplayRuleSet = itemDisplayRuleSet;
+        }
+
+
+    }
+}
---
+++ Starstorm 2/Survivors/Nemmando/NemmandoItemDisplays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starstorm2Unofficial.Survivors.Nemmando
+{
+    internal class NemmandoItemDisplays
+    {
+    }
+}
